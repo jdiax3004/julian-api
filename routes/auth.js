d9d139e32aabfbbfe7ef8d3e0695d6182a107038
Julian Diaz <julian.diaz.contacto@gmail.com>
2023-06-26 17:34:34 -0400

    First Commit
    Includes all source code

---
+++ routes/auth.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+// POST - User Registration
+router.post('/register', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      
+      // Generate a salt and hash the password
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      
+      // Create a new user with the hashed password
+      const newUser = new User({
+        email,
+        password: hashedPassword
+      });
+
+      // Save the user to the database
+      const savedUser = await newUser.save();
+  
+      res.status(200).json({ message: 'User registered successfully', user: savedUser });
+    } catch (error) {
+      console.error('Failed to register user:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
+// POST - User Login
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Compare the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Generate a JSON Web Token (JWT)
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// GET - Users
+router.get('/users', async (req, res) => {
+    try {
+      const users = await User.find({}, '-password');
+      res.status(200).json(users);
+    } catch (error) {
+      console.error('Failed to get users:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB:', err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// Middleware
+app.use(express.json());
+
+// Routes
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+
